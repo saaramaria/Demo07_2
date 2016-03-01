3a6246fd7834f3f8f5655bfed81531e2329250d1
Saara Virtanen <K1698@student.jamk.fi>
2016-03-01 15:36:30 +0200

    Homework

---
+++ LotteryApp/Lottery.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LotteryApp
+{
+    /// <summary>
+    /// Lottery-class
+    /// @author Saara Virtanen
+    /// @version 28.2.2016
+    /// </summary>
+    class Lottery
+    {
+        private static readonly Random rand = new Random();
+
+        public string generateNumbers()
+        {
+            // create a list of 39 numbers to pick the lottery numbers from
+            List<int> numbers = new List<int>();
+            for (int i = 1; i <= 39; i++)
+            {
+                numbers.Add(i);
+            }
+
+            // create an array for the lottery numbers (7)
+            int[] lotteryNumbers = new int[7];
+            for (int i = 0; i < lotteryNumbers.Length; i++)
+            {
+                // generate a random number
+                int number = rand.Next(0, numbers.Count);
+                // use number as an index in numbers-list and set that as the value of the current index of lotteryNumbers
+                lotteryNumbers[i] = numbers[number];
+                // remove number from numbers-list (no duplicates!)
+                numbers.RemoveAt(number);
+            }
+            
+            // convert lotteryNumbers to string-array strings, then join them into one string and return it
+            string[] strings = new string[lotteryNumbers.Length];
+            for (int i = 0; i < lotteryNumbers.Length; i++)
+                strings[i] = lotteryNumbers[i].ToString("00");
+
+            string result = string.Join("   ", strings);
+            return result;
+        }
+    }
+}
---
+++ LotteryApp/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace LotteryApp
+{
+    /// <summary>
+    /// Lottery app
+    /// @author Saara Virtanen
+    /// @version 28.2.2016
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private int rowno = 1;
+
+        public MainPage()
+        {
+            // draw UI from XAML-code
+            this.InitializeComponent();
+        }
+
+        private void drawNumbers_Click(object sender, RoutedEventArgs e)
+        {
+            // create a new lottery-object
+            Lottery lottery = new Lottery();
+            
+            // convert the text of drawCount to int
+            string drawString = drawCount.Text;
+            int draws = int.Parse(drawString);
+
+            // print the randomly generated rows to numberBlock
+            for (int i = 0; i < draws; i++)
+            {
+                numberBlock.Text += "Row " + rowno.ToString("00") + ":   " + lottery.generateNumbers() + "\n";
+                rowno++;
+            }
+        }
+
+        private void clearNumbers_Click(object sender, RoutedEventArgs e)
+        {
+            // clear numbers from numberBlock and set rownumber to 1
+            numberBlock.Text = "";
+            rowno = 1;
+        }
+    }
+}
